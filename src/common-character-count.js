5a9b5cdf890f9d57b3f0f385f7bd619505e54c40
Pavel <p.filippov2.20@gmail.com>
2023-12-12 17:57:20 +0200

    feat: start basic js task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+
+  let count = 0;
+  // Loop through each character in s1
+  for (let i = 0; i < s1.length; i++) {
+    // Check if the character exists in s2
+    const index = s2.indexOf(s1[i]);
+    if (index !== -1) {
+      // If it does, increment the count and remove the character from s2
+      count++;
+      s2 = s2.slice(0, index) + s2.slice(index + 1);
+    }
+  }
+  console.log(count)
+  return count;
 }
 
+
+getCommonCharacterCount('aabcc', 'adcaa')
+
 module.exports = {
-  getCommonCharacterCount
+    getCommonCharacterCount
 };
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const numString = n.toString();
+  let maxNumber = -99999999;
+
+  for (let i = 0; i < numString.length; i++) {
+    const newNumber = parseInt(numString.slice(0, i) + numString.slice(i + 1));
+    if (newNumber > maxNumber) {
+      console.log(newNumber)
+      console.log(maxNumber)
+      maxNumber = newNumber;
+      console.log(maxNumber)
+    }
+  }
+console.log(maxNumber + '  ' + 'max')
+  return maxNumber;
 }
+deleteDigit(21234)
+
 
 module.exports = {
   deleteDigit
